9b34bd2e5d3f8bb2ba4420df5dda846df8a2f866
Bata Zoltán <batazo.dad@gmail.com>
2024-04-16 17:37:01 +0200

    tesztJson, tesztHTML routes

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,16 +3,33 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController
 {
-    #[Route('/')]
+    #[Route('/', "home")]
     public function defaultAction(): Response
     {
         $number = random_int(0, 100);
 
         return new Response(
             '<html><body>Hello: '.$number.'</body></html>'
+        );
+    }
+
+    #[Route('/tesztJson', "teszt-json")]
+    public function tesztJsonAction(): JsonResponse
+    {
+        (array)$data = ['message'=>'OK'];
+
+        return new JsonResponse($data);
+    }
+
+    #[Route('/tesztHTML', "teszt-html")]
+    public function tesztHtmlAction(): Response
+    {
+        return new Response(
+            '<html><body>Hello</body></html>'
         );
     }
 }
