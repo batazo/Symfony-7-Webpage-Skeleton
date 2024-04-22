1f40c208e78f8b330e3feca55efbca30b801300d
Bata Zoltán <batazo.dad@gmail.com>
2024-04-22 23:01:05 +0200

    listener_path argument

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -11,9 +11,11 @@ class ExceptionListener
 {
 
     private $listener_type;
-    public function __construct($listener_type)
+    private $listener_path;
+    public function __construct($listener_type, $listener_path)
     {
         $this->listener_type = $listener_type;
+        $this->listener_path = $listener_path;
     }
     public function onKernelException(ExceptionEvent $event)
     {
@@ -21,21 +23,23 @@ class ExceptionListener
         $exception = $event->getThrowable();        
         $request   = $event->getRequest();
         $path = $request->getPathInfo();
-   
+        $expectedpath = $this->listener_path;
 
-        if (strpos($path, "/api") !== false)
+        if (strpos($path, $expectedpath) !== false)
         {
 
             $response = new JsonResponse([
                 'message'       => $exception->getMessage(),
                 'code'          => $exception->getStatusCode(),
-                'listener_type' => $this->listener_type
+                'listener_type' => $this->listener_type,
+                'listener_path' => $expectedpath
                 //'traces'        => $exception->getTrace()
             ]);
 
             if ($exception instanceof HttpExceptionInterface) {
                 $response->setStatusCode($exception->getStatusCode());
                 $response->headers->replace($exception->getHeaders());
+                $response->headers->set('Content-Type', 'application/json');
             } else {
                 $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
             }
